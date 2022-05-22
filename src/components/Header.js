324846ebb6ea59ec9e56a9d3a4206d5e30f64e69
Blizzcane <blizzcane@gmail.com>
2022-05-22 17:53:02 -0500

    added navbar

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Pantry Chef
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Button variant="outline-success">Sign up</Button>
+      </Container>
+    </Navbar>
+  );
+}
