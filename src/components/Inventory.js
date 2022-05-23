e3e7a09adbdc3b1720bc6d7b412213b8a270e9f4
Blizzcane <blizzcane@gmail.com>
2022-05-22 19:38:28 -0500

    random error about re renders popped up

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,11 +2,29 @@ import React, { useState } from "react";
 
 const Inventory = ({ ingredients }) => {
   const [filter, setFilter] = useState("");
+  const [inventory, setInventory] = useState([]);
+
+  const onAddHandler = (item) => {
+    setInventory((prevInv)=> [...prevInv,item])
+  };
 
   return (
     <div>
+      <div>
+        <ul className="list-group">
+          {inventory.map((item) => (
+            <li
+              className="list-group-item list-group-item-action"
+              key={item[1]} 
+            >
+              {item[0]}
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <p>
-        Type to filter the list: 
+        Type to filter the list:
         <input
           id="filter"
           name="filter"
@@ -15,11 +33,18 @@ const Inventory = ({ ingredients }) => {
           onChange={({ target }) => setFilter(target.value)}
         />
       </p>
-      <ul className="list-group" >
+      <ul className="list-group">
         {ingredients
           .filter((item) => item[0].includes(filter) || filter === "")
           .map((item) => (
-            <a href="#" className="list-group-item list-group-item-action"  key={item[1]}>{item[0]}</a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action"
+              key={item[1]}
+              onClick={()=>{onAddHandler(item)}}
+            >
+              {item[0]}
+            </a>
           ))}
       </ul>
     </div>
