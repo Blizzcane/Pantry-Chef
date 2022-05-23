1464dbaff8ac6119c8d3fa59e8fffb67fefa60a7
Blizzcane <blizzcane@gmail.com>
2022-05-22 19:04:30 -0500

    added filtered list input

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Inventory = ({ ingredients }) => { 
+const Inventory = ({ ingredients }) => {
+  const [filter, setFilter] = useState("");
+
   return (
     <div>
-      <ul> 
+      <p>
+        Type to filter the list: 
+        <input
+          id="filter"
+          name="filter"
+          type="text"
+          value={filter}
+          onChange={({ target }) => setFilter(target.value)}
+        />
+      </p>
+      <ul className="list-group" >
+        {ingredients
+          .filter((item) => item[0].includes(filter) || filter === "")
+          .map((item) => (
+            <a href="#" className="list-group-item list-group-item-action"  key={item[1]}>{item[0]}</a>
+          ))}
       </ul>
     </div>
   );
