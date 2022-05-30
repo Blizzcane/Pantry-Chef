979aaa3c165f9f9fd08976272491b3abe2bc4961
Blizzcane <blizzcane@gmail.com>
2022-05-29 19:26:43 -0500

    testing vercel

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,23 +4,25 @@ import "./Dashboard.css";
 
 const Dashboard = ({ pantry }) => {
   return (
-    <div className="dashboard">
-      <div>
-        <h3>Recipes</h3>
-        <Profile />
-      </div>
+    <>
+      <Profile />
+      <div className="dashboard">
+        <div>
+          <h3>Recipes</h3>
+        </div>
 
-      <div>
-        <h3>My Pantry</h3>
-        <ul className="list-group">
-          {pantry.map((item) => (
-            <li className="list-group-item list-group-item-action">
-              {item[0]}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h3>My Pantry</h3>
+          <ul className="list-group">
+            {pantry.map((item) => (
+              <li className="list-group-item list-group-item-action">
+                {item[0]}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
