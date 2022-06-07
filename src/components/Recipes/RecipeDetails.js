84ced0e45f327307049d63b848070d98931bca61
Blizzcane <blizzcane@gmail.com>
2022-06-06 21:39:04 -0500

    adding detailed view of recipe

---
+++ src/components/Recipes/RecipeDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const RecipeDetails = () => {
+  let navigate = useNavigate();
+
+  return <div>RecipeDetails</div>;
+};
+
+export default RecipeDetails;
--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Recipes = ({ recipes }) => {
   return (
-    <div className="d-flex flex-wrap justify-content-start">
+    <div className="d-flex flex-wrap justify-content-between">
       {recipes.map((recipe) => (
-        <div className="card m-1">
-          <div className="card-body">This is some text within a card body.</div>
+        <div key={recipe.idMeal} className="card m-1">
+          <div className="card-body">{recipe.strMeal}</div>
         </div>
       ))}
     </div>
