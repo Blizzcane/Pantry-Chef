c6bae74e6631cc85ae772c0468512b2dd3dedec3
Blizzcane <blizzcane@gmail.com>
2022-06-13 22:18:49 -0500

    added ingredients list

--- src/components/Recipes/RecipeDetails.js
+++ src/components/Recipes/RecipeDetails.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, ListGroup, ToggleButton } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 
 const RecipeDetails = () => {
   let navigate = useNavigate();
   let { recipeId } = useParams();
   const [recipe, setRecipe] = useState(null);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
+
+  const radios = [
+    { name: "Instructions", value: "1" },
+    { name: "Ingredients", value: "2" },
+  ];
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -41,10 +48,42 @@ const RecipeDetails = () => {
     );
 
   const steps = recipe.strInstructions.split(".");
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (recipe[`strIngredient${i}`] !== "") {
+      let name = recipe[`strIngredient${i}`];
+      let measure = recipe[`strMeasure${i}`];
+      ingredients.push({ name, measure });
+    }
+  }
+
+  const instructionsJSX = (
+    <ListGroup variant="flush" className="m-2" as="ol" numbered>
+      {steps.map((step) => (
+        <ListGroup.Item
+          as="li"
+          key={step.slice(0, 4)}
+          className="list-group-item"
+        >
+          {step}.
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+
+  const ingredientsJSX = (
+    <ListGroup variant="flush" className="m-2">
+      {ingredients.map((ingredient) => (
+        <ListGroup.Item>
+          {ingredient.measure + " " + ingredient.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 
   return (
-    <div>
-      <h1 className=" display-6 fw-normal">{recipe.strMeal.toUpperCase()}</h1>
+    <div >
+      <h1 className="text-center display-6 fw-normal">{recipe.strMeal.toUpperCase()}</h1>
 
       <div>
         <Button
@@ -61,16 +100,26 @@ const RecipeDetails = () => {
             className="shadow img-fluid img-thumbnail rounded float-start m-2"
             src={recipe.strMealThumb}
           />
-        </div>
-        <div>
-          <ul className="list-group list-group-flush list-group-numbered m-2">
-            {steps.map((step) => (
-              <li key={step.slice(0, 4)} className="list-group-item">
-                {step}.
-              </li>
+        </div>{" "}
+        <div className="text-center">
+          <ButtonGroup className="mt-2">
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant="outline-success"
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+              >
+                {radio.name}
+              </ToggleButton>
             ))}
-          </ul>
+          </ButtonGroup>{" "}
         </div>
+        <div>{radioValue === "1" ? instructionsJSX : ingredientsJSX}</div>
       </div>
     </div>
   );
