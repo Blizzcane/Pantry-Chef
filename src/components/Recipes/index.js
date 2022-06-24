a6a18be6bcdef19bd80caa29ed789a8cae17a902
Blizzcane <blizzcane@gmail.com>
2022-06-23 22:32:33 -0500

    trying to figure out layout for Recipe route

--- src/components/Recipes/index.js
+++ src/components/Recipes/index.js
@@ -3,11 +3,14 @@ import React from "react";
 const Recipes = ({ recipes }) => {
   return (
     <div className="d-flex flex-wrap justify-content-between">
-      {recipes.map((recipe) => (
-        <div key={recipe.idMeal} className="card m-1">
-          <div className="card-body">{recipe.strMeal}</div>
-        </div>
-      ))}
+      <ul>
+        {" "}
+        {recipes.map((recipe) => (
+          <li key={recipe.idMeal} className="card m-1">
+            <div className="card-body">{recipe.strMeal}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
