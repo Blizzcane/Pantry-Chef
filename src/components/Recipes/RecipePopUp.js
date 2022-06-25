162b2584525f5630b2d1f4111e6b7fe8a18c7251
Blizzcane <blizzcane@gmail.com>
2022-06-25 00:38:52 -0500

    editing css

--- src/components/Recipes/RecipePopUp.js
+++ src/components/Recipes/RecipePopUp.js
@@ -1,29 +1,40 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Collapse } from "react-bootstrap";
+import { Button, Card, Collapse, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import useFetch from "../../utils/useFetch";
 
-const RecipePopUp = ({ recipeId, open }) => { 
+const RecipePopUp = ({ recipeId, open }) => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
   const [recipe, error] = useFetch(url);
-  
+
   return (
-    <aside style={{ minHeight: "100vh" }}>
-      <Collapse in={open} dimension="width">
-        <div id="recipe-details">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={recipe[0]?.strMealThumb} />
-            <Card.Body>
-              <Card.Title>{recipe[0]?.strMeal}</Card.Title>
-              <Link to={`/recipe-details/${recipe[0]?.idMeal}`}>
-                <Button variant="success">View Full Recipe</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </div>
-      </Collapse>
-    </aside>
+    recipe && (
+      <aside style={{ minHeight: "100vh" }}>
+        <Collapse in={open} dimension="width">
+          <div id="recipe-details">
+            <Card style={{ width: "20rem" }}>
+              <Card.Img variant="top" src={recipe[0]?.strMealThumb} />
+              <Card.Body>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{recipe[0]?.strMeal}</Card.Title>
+                  </Card.Body>
+                </Card>
+                <Link
+                  className="d-grid gap-2"
+                  to={`/recipe-details/${recipe[0]?.idMeal}`}
+                >
+                  <Button variant="success" size="sm">
+                    View Full Recipe
+                  </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </Collapse>
+      </aside>
+    )
   );
 };
 
